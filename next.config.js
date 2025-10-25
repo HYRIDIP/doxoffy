79b9b7f294b9f5a1409d6eb0fb433221f362e65c
North <148585109+HYRIDIP@users.noreply.github.com>
2025-10-25 18:52:33 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,9 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-  images: {
-    domains: ['localhost'],
-  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        net: false,
+        tls: false
+      }
+    }
+    return config
+  }
 }
 
 module.exports = nextConfig
